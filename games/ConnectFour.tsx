d605bb8f16f4536ddce3beb4326f7228cf7d0d79
Alexander Pomareda <marlomp@hotmail.com>
2025-11-27 12:15:39 -0500

    Fix responsive at connect four

--- games/ConnectFour.tsx
+++ games/ConnectFour.tsx
@@ -128,7 +128,7 @@ export const ConnectFour: React.FC = () => {
   };
 
   return (
-    <GameLayout title="Connect Four" className="-mt-24 lg:-mt-60">
+    <GameLayout title="Connect Four" className="-mt-24 lg:-mt-60 xl:mt-0">
       <div className="flex flex-col items-center gap-6">
         <div className="bg-slate-800/50 px-6 py-2 rounded-full border border-white/10">
           <span className="text-slate-300 mr-2">Current Turn:</span>
@@ -142,13 +142,13 @@ export const ConnectFour: React.FC = () => {
         </div>
 
         <div className="bg-blue-800 p-2 lg:p-6 rounded-xl">
-          <div className="p-4 sm:p-6 lg:p-10 bg-zinc-50 rounded-xl shadow-2xl shadow-blue-900/50 border-4 border-blue-700 relative">
-            <div className="grid grid-cols-7 gap-4 sm:gap-6 md:gap-9 lg:gap-16 relative z-10">
+          <div className="p-4 sm:p-6 lg:p-10 xl:p-8 bg-zinc-50 rounded-xl shadow-2xl shadow-blue-900/50 border-4 border-blue-700 relative">
+            <div className="grid grid-cols-7 gap-4 sm:gap-6 md:gap-9 lg:gap-16 xl:gap-10 relative z-10">
               {/* Clickable columns logic */}
               {Array.from({ length: COLS }).map((_, colIndex) => (
                 <div
                   key={colIndex}
-                  className="flex flex-col gap-4 sm:gap-6 md:gap-9 lg:gap-16"
+                  className="flex flex-col gap-4 sm:gap-6 md:gap-9 lg:gap-16 xl:gap-10"
                   onClick={() => dropPiece(colIndex)}
                 >
                   {board.map((row, rowIndex) => {
@@ -160,7 +160,7 @@ export const ConnectFour: React.FC = () => {
                     return (
                       <div
                         key={`${rowIndex}-${colIndex}`}
-                        className="w-6 h-6 sm:w-7 sm:h-7 md:w-8 md:h-8 lg:w-10 lg:h-10 rounded-full border-[2px] border-blue-700 bg-slate-900 flex items-center justify-center cursor-pointer hover:ring-4 ring-white/10 transition-all relative overflow-visible"
+                        className="w-6 h-6 sm:w-7 sm:h-7 md:w-8 md:h-8 lg:w-10 lg:h-10 xl:w-8 xl:h-8 rounded-full border-[2px] border-blue-700 bg-slate-900 flex items-center justify-center cursor-pointer hover:ring-4 ring-white/10 transition-all relative overflow-visible"
                       >
                         {/* The Piece */}
                         {row[colIndex] && (
